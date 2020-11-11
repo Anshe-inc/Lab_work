eac90d8d143d2a56a961aa85483a18cd44e5cf26
Anshe-inc <shcherbakov.andrei@phystech.edu>
2020-11-11 17:53:29 +0300

    comments added

--- 1/main.c
+++ 1/main.c
@@ -2,13 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-char ** read(int * size);
-void sort(char ** string_arr, int size);
-void out_str(char ** string_arr);
+// прототипы функций
+char ** read(int * size);                   // считывает данные возвращает массив и размер
+void sort(char ** string_arr, int size);    // сортирует массив строк
+void out_str(char ** string_arr);           // вывод ланных
 
-int cmp(const void* a, const void * b);
-int int_num(char * string);
-void clear_arr(char ** string_arr, int size);
+int cmp(const void* a, const void * b);     // функция-сравниватель
+int int_num(char * string);                 // считает количество цифр
+void clear_arr(char ** string_arr, int size);// освобождает память после завершения программы
 
 int main(){
     char ** string_arr;
@@ -26,13 +27,14 @@ int main(){
 }
 
 char ** read(int * size){
-    char * string;
-    char ** res = malloc(sizeof(char *));
+    char * string;                              // для хранения массива
+    char ** res = malloc(sizeof(char *));       // выделение 1 элемента
     int max_len, arr_size = 1;
     int i;
     for (i = 0; scanf("%ms %d", &string, &max_len) > 0; ++i) {
-        //fprintf(stderr, "%s\n", string);
+        // считываем данные завершаемся, когда конец
         if(arr_size <= i){
+            // увеличиваем память при нехватке места в 2 раза
             res = realloc(res, 2 * arr_size * sizeof(char *));
             arr_size *= 2;
         }
@@ -41,22 +43,24 @@ char ** read(int * size){
         }
         res[i] = string;
     }
+    // переопределяем размер памяти так как 2^([log_2(N)] + 1) это много 
     res = realloc(res, i * sizeof(char *));
-
+    // записываем размер в size
     *size = i;
 
     return res;
 }
 
 int cmp(const void * a, const void * b){
+    // сравниваем строки по количеству цифр
+    // иначе лексикографически 
     char * str1 = *(char **)a;
     char * str2 = *(char **)b;
     int n1 = int_num(str1);
     int n2 = int_num(str2);
-    //printf("%d %d\n", n1, n2);
 
     if(n1 == n2){
-        return (int)str1[0] - (int)str2[0];
+        return strcmp(str1, str2);
     }
     else {
         return n1 - n2;
@@ -69,7 +73,7 @@ void sort(char ** string_arr, int size) {
 
 
 int int_num(char * string){
-    //printf("\n%s\n", string);
+    // функция подсчета количества цифр в строке
 
     int res = 0;
     for(char *c = string; *c != '\0'; ++c){
@@ -84,6 +88,8 @@ int int_num(char * string){
 void out_str(char ** string_arr){
     printf("%d ", int_num(string_arr[0]));
     char *c = string_arr[0];
+    // ищет 2 цифру в строке
+    // нет цифры -- -1
     for(int i = 0 ; ; ++c){
         if((*c - '0') < 10){
             ++i;
